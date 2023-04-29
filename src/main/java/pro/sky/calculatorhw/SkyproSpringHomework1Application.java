c27f446b43738095a5c36b20616ae8ef659283f1
pavelbesedin <pavelbesedin97@mail.ru>
2023-04-29 21:10:59 +0300

    Initial commit

---
+++ src/main/java/pro/sky/calculatorhw/SkyproSpringHomework1Application.java
@@ -0,0 +1,13 @@
+package pro.sky.calculatorhw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSpringHomework1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproSpringHomework1Application.class, args);
+	}
+
+}
